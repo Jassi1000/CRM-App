7f8639d95f69d8d1707bc80af1246a92d35c8e51
Jassi1000 <145197768+Jassi1000@users.noreply.github.com>
2025-09-13 18:10:55 +0530

    Update CreateSegment.js

--- Frontend/src/Component.js/CreateSegment.js
+++ Frontend/src/Component.js/CreateSegment.js
@@ -21,7 +21,7 @@ const CreateSegment = () => {
       setMessage("");
 
       const res = await axios.post(
-        "http://localhost:5000/api/v1/createSegment",
+        "https://crm-backend-bhuu.onrender.com/api/v1/createSegment",
         { query: segmentText },
         {
           headers: {
